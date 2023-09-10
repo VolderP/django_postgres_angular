c61420bdec91592145748072ba93fa1b2839c3a6
Pieter <Pieter@DESKTOP-TP89OFM>
2023-09-10 11:39:08 +0200

    first steps diverging from the default angular page

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -1,14 +1,22 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuestionnaireComponent,
+    ResultsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule // Make sure AppRoutingModule is in the imports array
   ],
   providers: [],
   bootstrap: [AppComponent]
